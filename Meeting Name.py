cb75ce871f31aef9a5bc806d06166ebf1c2cb176
Pinak Parate <65182151+PinskyLabs@users.noreply.github.com>
2021-10-29 22:38:32 +0530

    Delete Meeting Name.py

--- Meeting Name.py
+++
@@ -1,7 +0,0 @@
-
-meeting_name = input("Enter your Zoom username: ")
-
-if len(meeting_name) > 14:
-    print("This name will be viewed on your Zoom meeting name: " + ">> " + meeting_name[:14] + "..." + " <<")
-else:
-    print("This name will be viewed on your meeting name: " + ">> " + meeting_name + " <<")
