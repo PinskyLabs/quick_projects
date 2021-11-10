1c73cafb863b91b631233501b8318bb096b1ff1e
Pinak Parate <65182151+PinskyLabs@users.noreply.github.com>
2021-11-10 10:59:20 +0530

    Update Quadratic Formula.py

--- Quadratic Formula.py
+++ Quadratic Formula.py
@@ -1,6 +1,8 @@
 
 print("\nYour quadratic equation should be in this form: ax^2 + bx + c = 0\n")
 
+print("NOTE: This program will give you an error if your equation's roots are imaginary/complex")
+
 coeff_a = int(input("Enter 'a' from your equation: "))
 coeff_b = int(input("Enter 'b' from your equation: "))
 coeff_c = int(input("Enter 'c' from your equation: "))
