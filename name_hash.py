c0b361e7c7daec5da310a7ed7dfc9952645ee8df
Pinak Parate <65182151+PinskyLabs@users.noreply.github.com>
2024-06-19 20:00:38 +0530

    Update name_hash.py

--- name_hash.py
+++ name_hash.py
@@ -3,13 +3,13 @@ import hashlib
 name = input("Enter your name: ")
 name_list = list(name)
 
-# Convert each character to a SHA-256 hash and store in a list
+# Converting each letter to a hash and put it in a list
 hash_list = [hashlib.sha256(char.encode()).hexdigest() for char in name_list]
 
 # Concatenate the hashes into a single string
 concatenated_hashes = ''.join(hash_list)
 
-# Compute the SHA-256 hash of the concatenated string
+# Finding the hash of the concatenated string
 final_hash = hashlib.sha256(concatenated_hashes.encode()).hexdigest()
 
 print(final_hash)
